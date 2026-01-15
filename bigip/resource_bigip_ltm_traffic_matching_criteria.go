bf327bfa765f8d1f88769bd09cb2bcf75e0bb6d9
retryW <retryw01@gmail.com>
2026-01-16 00:02:59 +1100

    add resource for traffic matching criteria and partial address list

---
+++ bigip/resource_bigip_ltm_traffic_matching_criteria.go
@@ -0,0 +1,196 @@
+/*
+Original work from https://github.com/DealerDotCom/terraform-provider-bigip
+Modifications Copyright 2019 F5 Networks Inc.
+This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+If a copy of the MPL was not distributed with this file,You can obtain one at https://mozilla.org/MPL/2.0/.
+*/
+package bigip
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/f5devcentral/go-bigip"
+	bigip "github.com/f5devcentral/go-bigip"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceBigipLtmTrafficMatchingCriteria() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceBigipLtmTrafficMatchingCriteriaCreate,
+		ReadContext:   resourceBigipLtmTrafficMatchingCriteriaRead,
+		UpdateContext: resourceBigipLtmTrafficMatchingCriteriaUpdate,
+		DeleteContext: resourceBigipLtmTrafficMatchingCriteriaDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:         schema.TypeString,
+				Required:     true,
+				Description:  "Name of the Traffic Matching Criteria",
+				ForceNew:     true,
+				ValidateFunc: validateF5NameWithDirectory,
+			},
+			"description": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Description of the Traffic Matching Criteria",
+			},
+			"protocol": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "The transport protocol used in the Traffic Matching Criteria",
+			},
+			"route_domain": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "The route domain used in the Traffic Matching Criteria",
+			},
+			"destination_address_list": {
+				Type:          schema.TypeString,
+				Optional:      true,
+				Description:   "The name of an address list shared object to use for destination routing",
+				ConflictsWith: []string{"destination_address_inline"},
+			},
+			"destination_address_inline": {
+				Type:          schema.TypeString,
+				Optional:      true,
+				Description:   "A specific address to use for destination routing",
+				ConflictsWith: []string{"destination_address_list"},
+			},
+			"destination_port_list": {
+				Type:          schema.TypeString,
+				Optional:      true,
+				Description:   "The name of an port list shared object to use for destiantion port routing",
+				ConflictsWith: []string{"destination_port_inline"},
+			},
+			"destination_port_inline": {
+				Type:          schema.TypeString,
+				Optional:      true,
+				Description:   "A specific port to use for destination port routing",
+				ConflictsWith: []string{"destination_port_list"},
+			},
+			"source_address_list": {
+				Type:          schema.TypeString,
+				Optional:      true,
+				Description:   "The name of an address list shared object for source address matching",
+				ConflictsWith: []string{"source_address_inline"},
+			},
+			"source_address_inline": {
+				Type:          schema.TypeString,
+				Optional:      true,
+				Description:   "A specific address and optional mask to use for source matching",
+				ConflictsWith: []string{"source_address_inline"},
+			},
+			"source_port_inline": {
+				Type:        schema.TypeInt,
+				Optional:    true,
+				Description: "A specific port to use for source port matching",
+			},
+		},
+	}
+}
+
+func resourceBigipLtmTrafficMatchingCriteriaCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*bigip.BigIP)
+	name := d.Get("name").(string)
+
+	log.Println("[INFO] Creating traffic matching criteria" + name)
+	tmc := &bigip.TrafficMatchingCriteria{
+		Name:                     d.Get("name"),
+		Description:              d.Get("description"),
+		Protocol:                 d.Get("protocol"),
+		RouteDomain:              d.Get("route_domain"),
+		DestinationAddressList:   d.Get("destination_address_list"),
+		DestinationAddressInline: d.Get("destination_address_inline"),
+		DestinationPortList:      d.Get("destination_port_list"),
+		DestinationPortInline:    d.Get("destination_port_inline"),
+		SourceAddressList:        d.Get("source_address_list"),
+		SourceAddressInline:      d.Get("source_address_inline"),
+		SourcePortInline:         d.Get("source_port_inline"),
+	}
+
+	err := client.AddTrafficMatchingCriteria(ctx, tmc)
+	if err != nil {
+		return diag.FromErr(fmt.Errorf("error creating traffic matching criteria (%s): %s", name, err))
+	}
+
+	return resourceBigipLtmTrafficMatchingCriteriaRead(ctx, d, meta)
+}
+
+func resourceBigipLtmTrafficMatchingCriteriaRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*bigip.BigIP)
+	name := d.Id()
+	_ = d.Set("name", name)
+
+	log.Println("[INFO] Reading traffic matching criteria " + name)
+	tmc, err := client.ReadTrafficMatchingCriteria(name)
+	if err != nil && strings.Contains(err.Error(), "not found") {
+		log.Printf("[WARN] Traffic Matching Criteria (%s) not found, removing from state", name)
+		d.SetId("")
+		return nil
+	}
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	_ = d.Set("description", tmc.Description)
+	_ = d.Set("protocol", tmc.Protocol)
+	_ = d.Set("route_domain", tmc.RouteDomain)
+	_ = d.Set("destination_address_list", tmc.DestinationAddressList)
+	_ = d.Set("destination_address_inline", tmc.DestinationAddressInline)
+	_ = d.Set("destination_port_list", tmc.DestinationPortList)
+	_ = d.Set("destination_port_inline", tmc.DestinationPortInline)
+	_ = d.Set("source_address_list", tmc.SourceAddressList)
+	_ = d.Set("source_address_inline", tmc.SourceAddressInline)
+	_ = d.Set("source_port_inline", tmc.SourcePortInline)
+
+	return nil
+}
+
+func resourceBigipLtmTrafficMatchingCriteriaUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*bigip.BigIP)
+	name := d.Id()
+	tmc := &bigip.TrafficMatchingCriteria{
+		Description:              d.Get("description"),
+		Protocol:                 d.Get("protocol"),
+		RouteDomain:              d.Get("route_domain"),
+		DestinationAddressList:   d.Get("destination_address_list"),
+		DestinationAddressInline: d.Get("destination_address_inline"),
+		DestinationPortList:      d.Get("destination_port_list"),
+		DestinationPortInline:    d.Get("destination_port_inline"),
+		SourceAddressList:        d.Get("source_address_list"),
+		SourceAddressInline:      d.Get("source_address_inline"),
+		SourcePortInline:         d.Get("source_port_inline"),
+	}
+
+	fmt.Println("[INFO] Attempting to update traffic matching criteria ", name)
+	err := client.UpdateTrafficMatchingCriteria(name, tmc)
+	if err != nil {
+		fmt.Printf("[ERROR] Failed to update traffic matching criteria (%s) (%v)", name, err)
+		return diag.FromErr(err)
+	}
+
+	return resourceBigipLtmTrafficMatchingCriteriaRead(ctx, d, meta)
+}
+
+func resourceBigipLtmTrafficMatchingCriteriaDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*bigip.BigIP)
+	name := d.Id()
+
+	fmt.Println("[INFO] Attempting to delete traffing matching criteria " + name)
+	err := client.DeleteTrafficMatchingCriteria(name)
+	if err != nil {
+		fmt.Printf("[ERROR] Failed to delete traffic matching criteria (%s) (%v)", name, err)
+		return diag.FromErr(err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
---
+++ bigip/resource_bigip_net_address_list.go
@@ -0,0 +1,151 @@
+/*
+Original work from https://github.com/DealerDotCom/terraform-provider-bigip
+Modifications Copyright 2019 F5 Networks Inc.
+This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+If a copy of the MPL was not distributed with this file,You can obtain one at https://mozilla.org/MPL/2.0/.
+*/
+package bigip
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/f5devcentral/go-bigip"
+	bigip "github.com/f5devcentral/go-bigip"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceBigipNetAddressList() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceBigipNetAddressListCreate,
+		ReadContext:   resourceBigipNetAddressListRead,
+		UpdateContext: resourceBigipNetAddressListUpdate,
+		DeleteContext: resourceBigipNetAddressListDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:         schema.TypeString,
+				Required:     true,
+				Description:  "Name of the address list",
+				ForceNew:     true,
+				ValidateFunc: validateF5NameWithDirectory,
+			},
+			"description": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Description of the address list",
+			},
+			"addresses": {
+				Type:        schema.TypeList,
+				Optional:    true,
+				Description: "List of Addresses with mask",
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+		},
+	}
+}
+
+func resourceBigipNetAddressListCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*bigip.BigIP)
+	name := d.Get("name").(string)
+
+	log.Println("[INFO] Creating address list" + name)
+	addressList := &bigip.AddressList{
+		Name:        d.Get("name").(string),
+		Description: d.Get("description").(string),
+		Addresses:   d.Get("addresses"),
+	}
+
+	err := client.AddAddressList(ctx, addressList)
+	if err != nil {
+		return diag.FromErr(fmt.Errorf("error creating address list (%s): %s", name, err))
+	}
+
+	return resourceBigipNetAddressListRead(ctx, d, meta)
+}
+
+func resourceBigipNetAddressListRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*bigip.BigIP)
+	name := d.Id()
+	_ = d.Set("name", name)
+
+	log.Println("[INFO] Reading address list " + name)
+	addressList, err := client.GetAddressList(ctx, name)
+	if err != nil && strings.Contains(err.Error(), "not found") {
+		log.Printf("[WARN] address list (%s) not found, removing from state", name)
+		d.SetId("")
+		return nil
+	}
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	_ = d.Set("description", addressList.Description)
+	_ = d.Set("addresses", addressList.Addresses)
+
+	return nil
+}
+
+func resourceBigipNetAddressListUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*bigip.BigIP)
+	name := d.Id()
+	addressList := &bigip.AddressList{
+		Description: d.Get("description").(string),
+		Addresses:   listToAddressesListAddresses(d.Get("addresses").([]interface{})),
+	}
+
+	fmt.Println("[INFO] Attempting to update address list ", name)
+	err := client.ModifyAddressList(ctx, name, addressList)
+	if err != nil {
+		fmt.Printf("[ERROR] Failed to update address list (%s) (%v)", name, err)
+		return diag.FromErr(err)
+	}
+
+	return resourceBigipNetAddressListRead(ctx, d, meta)
+}
+
+func resourceBigipNetAddressListDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*bigip.BigIP)
+	name := d.Id()
+
+	fmt.Println("[INFO] Attempting to delete traffing matching criteria " + name)
+	err := client.DeleteAddressList(ctx, name)
+	if err != nil {
+		fmt.Printf("[ERROR] Failed to delete address list (%s) (%v)", name, err)
+		return diag.FromErr(err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func addressListFromConfig(d *schema.ResourceData) *bigip.AddressList {
+	al := &bigip.AddressList{
+		Name:        d.Id(),
+		Description: d.Get("description").(string),
+	}
+
+	if p, ok := d.GetOk("addresses"); ok {
+		al.Addresses = listToAddressesListAddresses(p.([]interface{}))
+	}
+
+	return al
+}
+
+func listToAddressesListAddresses(list []interface{}) []bigip.AddressListAddress {
+	ala := make([]bigip.AddressListAddress, len(list))
+	for i, v := range list {
+		ala[i].Name = v.(string)
+	}
+
+	return ala
+}
